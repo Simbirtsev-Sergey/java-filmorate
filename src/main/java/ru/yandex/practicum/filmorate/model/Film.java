5f3c587d4b06d027609fd8a786e549c14c5548aa
Sergey-Simbirtsev <Seregasimba@yandex.ru>
2026-09-10 23:41:11 +0300

    Сделать недочеты в моделях

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,8 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+
 import lombok.Data;
 import ru.yandex.practicum.filmorate.serializer.DurationSerializer;
 
@@ -11,14 +10,10 @@ import java.time.LocalDate;
 
 @Data
 public class Film {
-    @NotNull
     private Long id;
-    @NotBlank
     private String name;
-    @NotBlank
     private String description;
     private LocalDate releaseDate;
     @JsonSerialize(using = DurationSerializer.class)
-    @NotNull
     private Duration duration;
 }
